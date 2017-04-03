d5ad49e8192045cbce6e759f586bf21b26993094
YmraNaissur <daily.spb@yandex.ru>
2017-04-03 10:58:04 +0300

    Создал enum с типами платежей

---
+++ src/main/java/ru/naissur/payments/model/PaymentType.java
@@ -0,0 +1,13 @@
+package ru.naissur.payments.model;
+
+/**
+ * naissur
+ * 03.04.2017
+ */
+
+public enum PaymentType {
+    RENTAL, // арендная плата
+    UTILITY, // коммунальные услуги
+    ELECTRICITY, // электричество
+    RENEWAL // капитальный ремонт
+}
