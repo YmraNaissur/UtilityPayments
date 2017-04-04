50ed071d5fbc0c44edef46989739a9eff3b4d79e
YmraNaissur <daily.spb@yandex.ru>
2017-04-04 11:33:57 +0300

    Переопределил метод toString класса PaymentType

--- src/main/java/ru/naissur/payments/model/PaymentType.java
+++ src/main/java/ru/naissur/payments/model/PaymentType.java
@@ -9,5 +9,25 @@ public enum PaymentType {
     RENTAL, // арендная плата
     UTILITY, // коммунальные услуги
     ELECTRICITY, // электричество
-    RENEWAL // капитальный ремонт
+    RENEWAL; // капитальный ремонт
+
+    /*
+     *  Вернем человеческое наименование вида платежа
+     *  в зависимости от значения PaymentType
+     */
+    @Override
+    public String toString() {
+        switch (this) {
+            case RENTAL:
+                return "Арендная плата";
+            case UTILITY:
+                return "Коммунальные услуги";
+            case ELECTRICITY:
+                return "Электричество";
+            case RENEWAL:
+                return "Капитальный ремонт";
+            default:
+                return null;
+        }
+    }
 }
