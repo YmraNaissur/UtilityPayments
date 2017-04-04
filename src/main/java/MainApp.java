dcf8f22b43395b28ab30be3e02b34bbb8f475857
YmraNaissur <daily.spb@yandex.ru>
2017-04-04 11:50:58 +0300

    Проверил вывод всех четырех типов платежей

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,14 +1,25 @@
 import ru.naissur.payments.model.Payment;
 import ru.naissur.payments.model.PaymentType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * naissur
  * 03.04.2017
  */
 public class MainApp {
     public static void main(String[] args) {
-        Payment payment = new Payment(PaymentType.RENTAL, 9000);
-        System.out.println("Тип платежа: " + payment.getType());
-        System.out.println("Размер платежа: " + payment.getAmount());
+        List<Payment> monthlyPayments = new ArrayList<>();
+        monthlyPayments.add(new Payment(PaymentType.RENTAL, 9000));
+        monthlyPayments.add(new Payment(PaymentType.ELECTRICITY, 286));
+        monthlyPayments.add(new Payment(PaymentType.UTILITY, 3256.75));
+        monthlyPayments.add(new Payment(PaymentType.RENEWAL, 189.91));
+
+        monthlyPayments.forEach(p -> System.out.printf("Тип: %s, сумма: %.2f%n", getTypeInLowerCase(p.getType().toString()), p.getAmount()));
+    }
+
+    private static String getTypeInLowerCase(String type) {
+        return type.toLowerCase();
     }
 }
