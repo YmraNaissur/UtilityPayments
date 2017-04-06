4bc4fd4c7c9a0e093d284ad9694aaf341bb48517
YmraNaissur <daily.spb@yandex.ru>
2017-04-06 11:45:11 +0300

    Объявил методы get, getAll, save в PaymentRepository, создал реализацию in memory

--- src/main/java/ru/naissur/payments/repository/PaymentRepository.java
+++ src/main/java/ru/naissur/payments/repository/PaymentRepository.java
@@ -1,9 +1,25 @@
 package ru.naissur.payments.repository;
 
+import ru.naissur.payments.model.Payment;
+import java.util.List;
+
 /**
  * naissur
  * 06.04.2017
  */
 public interface PaymentRepository {
-    // TODO написать сигнатуры будущих методов
+    Payment save(Payment payment); // сохраняем платеж в хранилище
+
+    /**
+     * Получаем платеж из хранилища по id
+     * @param id идентификационный номер платежа
+     * @return платеж с указанным id или null, если такого id нет в хранилище
+     */
+    Payment get(int id);
+
+    /**
+     * Получаем все платежи из хранилища
+     * @return список всех платежей, которые есть в хранилище
+     */
+    List<Payment> getAll();
 }
--- src/main/java/ru/naissur/payments/repository/mock/InMemoryPaymentRepository.java
+++ src/main/java/ru/naissur/payments/repository/mock/InMemoryPaymentRepository.java
@@ -1,11 +1,46 @@
 package ru.naissur.payments.repository.mock;
 
+import ru.naissur.payments.model.Payment;
+import ru.naissur.payments.model.PaymentType;
 import ru.naissur.payments.repository.PaymentRepository;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * naissur
  * 06.04.2017
  */
 public class InMemoryPaymentRepository implements PaymentRepository {
-    // TODO сделать фейковое хранилище + реализовать методы интерфейса
+    private Map<Integer, Payment> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        save(new Payment(PaymentType.RENTAL, 9000, LocalDate.of(2017, 4, 10)));
+        save(new Payment(PaymentType.ELECTRICITY, 286, LocalDate.of(2017, 4, 30)));
+        save(new Payment(PaymentType.UTILITY, 3256.75, LocalDate.of(2017, 4, 20)));
+        save(new Payment(PaymentType.RENEWAL, 189.91, LocalDate.of(2017, 4, 10)));
+    }
+
+    @Override
+    public Payment save(Payment payment) {
+        if (payment.isNew()) {
+            payment.setId(counter.incrementAndGet());
+        }
+        repository.put(payment.getId(), payment);
+        return payment;
+    }
+
+    @Override
+    public Payment get(int id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public List<Payment> getAll() {
+        return repository.values().stream().collect(Collectors.toList());
+    }
 }
