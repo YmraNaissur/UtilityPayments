65c79997b4e88289f645b1193cbf5eee08b1b159
YmraNaissur <daily.spb@yandex.ru>
2017-04-06 11:18:22 +0300

    Добавил заготовку in memory реализации интерфейса PaymentRepository

---
+++ src/main/java/ru/naissur/payments/repository/mock/InMemoryPaymentRepository.java
@@ -0,0 +1,11 @@
+package ru.naissur.payments.repository.mock;
+
+import ru.naissur.payments.repository.PaymentRepository;
+
+/**
+ * naissur
+ * 06.04.2017
+ */
+public class InMemoryPaymentRepository implements PaymentRepository {
+    // TODO сделать фейковое хранилище + реализовать методы интерфейса
+}
